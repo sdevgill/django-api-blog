0a455cb18819e7daaaf0c331dc9a3588a488713c
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-10-15 15:59:54 +0100

    Add default routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from .views import PostList, PostDetail, UserList, UserDetail
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path("users/", UserList.as_view()),
-    path("users/<int:pk>/", UserDetail.as_view()),
-    path("", PostList.as_view(), name="post_list"),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-]
+router = DefaultRouter()
+router.register("users", UserViewSet, basename="user")
+router.register("", PostViewSet, basename="post")
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("users/", UserList.as_view()),
+#     path("users/<int:pk>/", UserDetail.as_view()),
+#     path("", PostList.as_view(), name="post_list"),
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+# ]
